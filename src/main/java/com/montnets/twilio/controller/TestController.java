ca9fe3f56d5c8eb283c30c3e2decfabc9feab2a7
黄志斌 <307260621@qq.com>
2019-05-24 18:59:37 +0800

    new

--- src/main/java/com/montnets/twilio/controller/TestController.java
+++ src/main/java/com/montnets/twilio/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.montnets.twilio.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +19,18 @@ public class TestController {
 	
 	@RequestMapping(value = "/sms", produces = MediaType.APPLICATION_XML_VALUE)
 	@ResponseBody
-	public Object sms() {
-		String content = "Hi! It looks like your phone number was born in {{ FromCountry }}";
+	public Object sms(HttpServletRequest req) {
+		String FromCountry = req.getParameter("FromCountry");
+		//String reqbody = getRequestBody(req);
+		String content = "your phone number was born in "+FromCountry;
 		Body body = new Body
                 .Builder(content)
                 .build();
         Message sms = new Message
-                .Builder()
-                .body(body)
+                .Builder(content)
+                //.body(body)
+                //.from("+16504580008")
+                //.to("+16504580008")
                 .build();
         MessagingResponse twiml = new MessagingResponse
                 .Builder()
@@ -29,6 +38,7 @@ public class TestController {
                 .build();
         return twiml.toXml();
 	}
+	
 	
 	
 }
