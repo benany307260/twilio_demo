2b6ed6d6779f337faef86b370aa2382df711e657
黄志斌 <307260621@qq.com>
2019-05-28 20:48:23 +0800

    new

--- src/main/java/com/montnets/twilio/controller/VoiceController.java
+++ src/main/java/com/montnets/twilio/controller/VoiceController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Conference;
 import com.twilio.twiml.voice.Dial;
 import com.twilio.twiml.voice.Say;
 
@@ -21,6 +22,46 @@ public class VoiceController {
 	@RequestMapping(value = "/voice", produces = MediaType.APPLICATION_XML_VALUE)
 	@ResponseBody
 	public Object voice(HttpServletRequest req) {
+		
+		//return dial();
+		
+		return conference(req);
+
+	}
+	
+	// Update with your own phone number in E.164 format
+	public static final String MODERATOR = "+15558675310";
+	
+	private Object conference(HttpServletRequest req) {
+		// Get the number of the incoming caller
+	    String fromNumber = req.getParameter("From");
+
+	    Conference.Builder conferenceBuilder = new Conference.Builder("My Conference");
+
+	    if (MODERATOR.equalsIgnoreCase(fromNumber)) {
+	      conferenceBuilder.startConferenceOnEnter(true);
+	      conferenceBuilder.endConferenceOnExit(true);
+	    } else {
+	      conferenceBuilder.endConferenceOnExit(false);
+	    }
+
+	    // Create a TwiML builder object
+	    VoiceResponse twiml = new VoiceResponse.Builder()
+	        .dial(new Dial.Builder()
+	              .conference(conferenceBuilder.build())
+	              .build()
+	        ).build();
+
+	    try {
+        	
+            return twiml.toXml();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+	}
+	
+	private Object dial() {
 		Dial dial = new Dial.Builder("+13125486404").build();
         Say say = new Say.Builder("Goodbye").build();
         VoiceResponse response = new VoiceResponse.Builder().dial(dial)
@@ -34,7 +75,6 @@ public class VoiceController {
             return null;
         }
 	}
-	
 	
 	
 	
