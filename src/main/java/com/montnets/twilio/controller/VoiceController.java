a094178052bde9f42a3bdf97de6ca0a0d8442f5b
黄志斌 <307260621@qq.com>
2019-05-29 11:55:00 +0800

    new

--- src/main/java/com/montnets/twilio/controller/VoiceController.java
+++ src/main/java/com/montnets/twilio/controller/VoiceController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.twilio.twiml.VoiceResponse;
 import com.twilio.twiml.voice.Conference;
 import com.twilio.twiml.voice.Dial;
+import com.twilio.twiml.voice.Reject;
 import com.twilio.twiml.voice.Say;
 
 @Controller
@@ -25,12 +26,52 @@ public class VoiceController {
 		
 		//return dial();
 		
-		return conference(req);
+		//return conference(req);
+		
+		return conferenceSpecificPhone(req);
 
 	}
 	
-	// Update with your own phone number in E.164 format
-	public static final String MODERATOR = "+15558675310";
+	private Object conferenceSpecificPhone(HttpServletRequest req) {
+		// Get the number of the incoming caller
+	    String fromNumber = req.getParameter("From");
+	    
+	    log.info("fromNumber="+fromNumber);
+	    
+	    String SpecificPhone = "+15017250604";
+
+	    VoiceResponse twiml = null;
+	    if(!SpecificPhone.equalsIgnoreCase(fromNumber)) {
+	    	Reject reject = new Reject.Builder().build();
+	    	twiml = new VoiceResponse.Builder()
+	        	.reject(reject)
+	            .build();
+	    }
+	    else {
+	    	Conference.Builder conferenceBuilder = new Conference.Builder("My Conference SpecificPhone");
+	    	String MODERATOR = "+15558675310";
+	    	if (MODERATOR.equalsIgnoreCase(fromNumber)) {
+	    		conferenceBuilder.startConferenceOnEnter(true);
+	    		conferenceBuilder.endConferenceOnExit(true);
+	    	} else {
+	    		conferenceBuilder.endConferenceOnExit(false);
+	    	}
+	    	
+	    	// Create a TwiML builder object
+	    	twiml = new VoiceResponse.Builder()
+	    			.dial(new Dial.Builder().conference(conferenceBuilder.build()).build())
+	    			.build();
+	    }
+	    
+	    try {
+        	
+            return twiml.toXml();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+	}
+	
 	
 	private Object conference(HttpServletRequest req) {
 		// Get the number of the incoming caller
@@ -38,6 +79,7 @@ public class VoiceController {
 
 	    Conference.Builder conferenceBuilder = new Conference.Builder("My Conference");
 
+	    String MODERATOR = "+15558675310";
 	    if (MODERATOR.equalsIgnoreCase(fromNumber)) {
 	      conferenceBuilder.startConferenceOnEnter(true);
 	      conferenceBuilder.endConferenceOnExit(true);
