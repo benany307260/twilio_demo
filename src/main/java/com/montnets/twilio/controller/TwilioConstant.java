03edc997d8dd47624266afd7f7890f96585caf9d
黄志斌 <307260621@qq.com>
2019-06-03 14:20:25 +0800

    new

--- src/main/java/com/montnets/twilio/controller/TwilioConstant.java
+++ src/main/java/com/montnets/twilio/controller/TwilioConstant.java
@@ -1,11 +1,11 @@
 package com.montnets.twilio.controller;
 
 public class TwilioConstant {
-/*	public static final String ACCOUNT_SID = "AC5ca4e322f2f240db9462a21c56d3bbe4";
-	public static final String AUTH_TOKEN = "56eab5b6da28c54707aa2863804acfc0";
+/*	public static final String ACCOUNT_SID = "";
+	public static final String AUTH_TOKEN = "";
 */
-	public static final String ACCOUNT_SID = "ACac1529992a70afb40a250700c606c5da";
-	public static final String AUTH_TOKEN = "1b231a1bfa290c5b2568874de0d897ef";
+	public static final String ACCOUNT_SID = "";
+	public static final String AUTH_TOKEN = "";
 	
 	public static final String NOTIFY_SID_HZB = "IS382b69e220afed9070cfd276926b2f96";
 
